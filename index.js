d047650017a86e3f4c431e5c4cfb6c845b3886e8
Isabel Paredes <iparedes314@gmail.com>
2023-01-24 14:52:44 +0100

    Clean up

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-class Queue {
-    constructor() {
-        this.elements = {};
-        this.head = 0;
-        this.tail = 0;
-    }
-    enqueue(element) {
-        console.log("QUEUE: " + element.data);
-        this.elements[this.tail] = element;
-        this.tail++;
-    }
-    dequeue() {
-        const item = this.elements[this.head];
-        delete this.elements[this.head];
-        this.head++;
-        return item;
-    }
-    peek() {
-        return this.elements[this.head];
-    }
-    get length() {
-        return this.tail - this.head;
-    }
-    get isEmpty() {
-        return this.length === 0;
-    }
-}
-  
-// let msg_queue = new Queue();
-let last_message = {"data": "data: I'm an empty message"};
-let new_message;
-
-// Communication channel
-const channel = new MessageChannel();
-
 // SUBSCRIBER
 
 let listener;
@@ -44,8 +9,6 @@ function startListener() {
     if (typeof(listener) == "undefined") {
         listener = new Worker("pubsub/listener.js");
     }
-
-    listener.postMessage({port: channel.port1}, [channel.port1]);
 
 }
 
@@ -59,13 +22,12 @@ function clearListener() {
     document.getElementById("listenerOutput").innerHTML = "";
 }
 
+
 // PUBLISHER 
 
 let publisher;
 
 function startTalker() {
-
-    startListener();
 
     document.getElementById("talkerOutput").innerHTML = "Initializing publisher...\n";
 
@@ -73,24 +35,6 @@ function startTalker() {
         publisher = new Worker("pubsub/talker.js");
     }
 
-    publisher.postMessage({port: channel.port2}, [channel.port2]);
-
-    publisher.addEventListener('message', function(e) {
-        const {port} = e.data;
-        port.postMessage(['hellow', 'monde']);
-    })
-
-    // publisher.onmessage = function(event) {
-    //     // msg_queue.enqueue(event);
-    //     new_message = event.data;
-    //     document.getElementById("talkerOutput").innerHTML += event.data;
-
-    //     if (typeof listener != "undefined") {
-    //         console.log(`[FROM PUB] ${new_message}`);
-    //         listener.postMessage(new_message);
-    //         console.log("[FROM PUB] results???");
-    //     }
-    // }
 }
 
 function stopTalker() {
